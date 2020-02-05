9edf8d8532379ee4cbfa8c5ac0d9b230ae1519c9
Buckles <doghusky1990@gmail.com>
2020-02-05 19:59:05 -0400

    moving away from an external picker

--- js/fapicker.js
+++ js/fapicker.js
@@ -1,8 +1,32 @@
 (function($) {
   $.entwine(function($) {
-    $(".fapicker.text").entwine({
+    $(".fa-holder .fapicker.text").entwine({
       onmatch: function() {
-        $(this).iconpicker();
+        let self = $(this);
+
+        //loop through all the icons
+        let newIcon = null;
+        for (i = 0; i < iconList.icons.length; i++) {
+          //make the new icon
+          newIcon =
+            '<div class="fapicker-icons__holder__icon" data-icon="' +
+            iconList.icons[i].title +
+            '"><i class="' +
+            iconList.icons[i].title +
+            '"></i></div>';
+
+          //add the icons
+          $(this)
+            .parent()
+            .find(".fapicker-icons .fapicker-icons__holder")
+            .append(newIcon);
+        }
+
+        //add the listeners
+        $(".fapicker-icons__holder__icon").click(function(e) {
+          console.log($(this).data("icon"));
+          self.val($(this).data("icon"));
+        });
       },
     });
 
--- templates/BucklesHusky/Forms/FAPickerField.ss
+++ templates/BucklesHusky/Forms/FAPickerField.ss
@@ -1,8 +1,23 @@
-<% require javascript(buckleshusky/silverstripe-fa-picker:js/external/fontawesome-iconpicker.js) %>
+
+<% require javascript(buckleshusky/silverstripe-fa-picker:js/icons.js) %>
 <% require javascript(buckleshusky/silverstripe-fa-picker:js/fapicker.js) %>
-<% require css(buckleshusky/silverstripe-fa-picker:css/external/fontawesome-iconpicker.min.css) %>
+<% require css(buckleshusky/silverstripe-fa-picker:css/fa-styles.css) %>
 <% require css(buckleshusky/silverstripe-fa-picker:css/external/fontawesome-all.min.css) %>
-<div>
+
+
+<div class="fa-holder">
     <input $AttributesHTML />
+
+    <div class="fapicker-icons">
+        <div class="fapicker-icons__search-holder">
+
+        </div>
+
+        <div class="fapicker-icons__holder">
+            
+        </div>
+    </div>
 </div>
+
+
 
