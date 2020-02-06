eae9e8da70edbf0f7203a9210816feeb0627cf25
DoggersHusky <robert.ce@webbuildersgroup.com>
2020-02-06 09:51:59 -0400

    fixed scooping

--- js/fapicker.js
+++ js/fapicker.js
@@ -1,18 +1,18 @@
 (function($) {
   $.entwine(function($) {
-    $(".fa-holder .fapicker.text").entwine({
+    $(".fa-holder").entwine({
       onmatch: function() {
         let self = $(this);
 
         //update icon ammount
-        $(".fapicker-icons__bottom > span:nth-child(2)  strong").html(
-          iconList.length,
-        );
+        $(this)
+          .find(".fapicker-icons__bottom > span:nth-child(2)  strong")
+          .html(iconList.length);
 
         //update icon version
-        $(".fapicker-icons__bottom > span:nth-child(1)  strong").html(
-          fa_version,
-        );
+        $(this)
+          .find(".fapicker-icons__bottom > span:nth-child(1)  strong")
+          .html(fa_version);
 
         //loop through all the icons
         let newIcon = null;
@@ -27,47 +27,49 @@
 
           //add the icons
           $(this)
-            .parent()
             .find(".fapicker-icons .fapicker-icons__holder")
             .append(newIcon);
         }
 
         //add the listeners for clicking the button
-        $(".fapicker-icons__holder__icon").click(function(e) {
-          //add the icon to the textfield
-          self.val($(this).data("icon"));
+        $(this)
+          .find(".fapicker-icons__holder__icon")
+          .click(function(e) {
+            //add the icon to the textfield
+            self.find("input.fapicker.text").val($(this).data("icon"));
 
-          //update slected icon
-          $("span.fapicker-icons__holder__icon i").attr(
-            "class",
-            $(this).data("icon"),
-          );
-        });
+            //update slected icon
+            self
+              .find("span.fapicker-icons__holder__icon i")
+              .attr("class", $(this).data("icon"));
+          });
 
         //search filter
-        $(".fapicker-icons__search-holder input").keyup(function(e) {
-          //reset icons after each key to allow for better filtering
-          self
-            .parent()
-            .find(".fapicker-icons .fapicker-icons__holder li")
-            .css("display", "flex");
+        $(this)
+          .find(".fapicker-icons__search-holder input")
+          .keyup(function(e) {
+            //reset icons after each key to allow for better filtering
+            self
+              .parent()
+              .find(".fapicker-icons .fapicker-icons__holder li")
+              .css("display", "flex");
 
-          //set newSearch to the val of the input
-          let newSearch = "" + $(this).val();
+            //set newSearch to the val of the input
+            let newSearch = "" + $(this).val();
 
-          //filter the list of icons
-          self
-            .parent()
-            .find(".fapicker-icons .fapicker-icons__holder li")
-            .filter(function(index) {
-              //check to see if the datta icon contains the searched word
-              //inverse to hide everything else
-              return !$(this)
-                .data("icon")
-                .includes(newSearch);
-            })
-            .css("display", "none");
-        });
+            //filter the list of icons
+            self
+              .parent()
+              .find(".fapicker-icons .fapicker-icons__holder li")
+              .filter(function(index) {
+                //check to see if the datta icon contains the searched word
+                //inverse to hide everything else
+                return !$(this)
+                  .data("icon")
+                  .includes(newSearch);
+              })
+              .css("display", "none");
+          });
       },
     });
   });
