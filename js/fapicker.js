f1a09007c0b125f9ebfd3337d0cda0db888fc015
Buckles <doghusky1990@gmail.com>
2020-09-15 20:12:49 -0300

    fix for multiple fontawesome pickers

--- js/fapicker.js
+++ js/fapicker.js
@@ -1,133 +1,116 @@
 (function ($) {
-    $.entwine(function ($) {
-        //the holder
-        $(".fa-holder").entwine({
-            Type: "",
-            onmatch: function () {
-                //update selected icon on start
-                $(this)
-                    .find("span.fapicker-icons__holder__icon i")
-                    .attr("class", $(this).find("input.fapicker.text").val());
+  $.entwine(function ($) {
+    $(".fa-holder").entwine({
+      Type: "",
+      onmatch: function () {
+        //reference
+        let self = $(this);
+        let activeIcon = $(this)
+          .find("input.fapicker.text")
+          .val()
+          .split(" ")
+          .pop();
 
-                //add active class to icon
-                $(this)
-                    .find(
-                        ".fapicker-icons__holder ." +
-                            $(this)
-                                .find("input.fapicker.text")
-                                .val()
-                                .split(" ")
-                                .pop()
-                    )
-                    .parent()
-                    .addClass("active");
-            },
-        });
+        //update selected icon on start
+        $(this)
+          .find("span.fapicker-icons__holder__icon i")
+          .attr("class", $(this).find("input.fapicker.text").val());
 
-        //icon clicking
-        $(".fapicker-icons__holder__icon").entwine({
-            onclick: function () {
-                //get the holder
-                let holder = $(".fa-holder");
+        //add active class to icon if has one
+        if (activeIcon) {
+          $(this)
+            .find(
+              ".fapicker-icons__holder ." +
+                $(this).find("input.fapicker.text").val().split(" ").pop(),
+            )
+            .parent()
+            .addClass("active");
+        }
 
-                //add the icon to the textfield
-                holder.find("input.fapicker.text").val($(this).data("icon"));
-
-                //hide all other actives
-                holder
-                    .find(".fapicker-icons__holder__icon.active")
-                    .removeClass("active");
-
-                //set active
-                $(this).addClass("active");
-
-                //update selected icon
-                holder
-                    .find("span.fapicker-icons__holder__icon i")
-                    .attr("class", $(this).data("icon"));
-            },
-        });
+        //clicking the icon event
+        $(this)
+          .find(".fapicker-icons__holder__icon")
+          .click(function () {
+            //add the icon to the textfield
+            self.find("input.fapicker.text").val($(this).data("icon"));
+            //hide all other actives
+            self
+              .find(".fapicker-icons__holder__icon.active")
+              .removeClass("active");
+            //set active
+            $(this).addClass("active");
+            //update selected icon
+            self
+              .find("span.fapicker-icons__holder__icon i")
+              .attr("class", $(this).data("icon"));
+          });
 
         //search filter
-        $(".fapicker-icons__search-holder input").entwine({
-            onkeyup: function (e) {
-                //run the search
-                this.doSearch();
-            },
-            doSearch: function () {
-                //get the holder
-                let holder = $(".fa-holder");
-                //get the type
-                let type = $(".fa-holder").getType();
-
-                //remove icon not in search class
-                holder
-                    .find(".fapicker-icons .fapicker-icons__holder li")
-                    .removeClass("iconNotSearch");
-
-                //set newSearch to the val of the input
-                let newSearch = "" + $(this).val();
-
-                //filter the list of icons
-                holder
-                    .find(
-                        ".fapicker-icons .fapicker-icons__holder li div:first-child"
-                    )
-                    .filter(function (index) {
-                        //check to see if the data icon contains the searched word
-                        //inverse to hide everything else
-                        return !$(this).data("icon").includes(newSearch);
-                    })
-                    .parent()
-                    .addClass("iconNotSearch");
-            },
-            doFilterType: function () {
-                //get the holder
-                let holder = $(".fa-holder");
-                //get the type
-                let type = $(".fa-holder").getType() + " ";
-
-                // //remove class
-                holder
-                    .find(".fapicker-icons .fapicker-icons__holder li")
-                    .removeClass("notInSearch");
-
-                //do we have a type
-                if (type.trim()) {
-                    //filter the list of icons
-                    holder
-                        .find(
-                            ".fapicker-icons .fapicker-icons__holder li div:first-child"
-                        )
-                        .filter(function (index) {
-                            //check to see if the data icon contains the searched word
-                            //inverse to hide everything else
-                            return !$(this).data("icon").includes(type);
-                        })
-                        .parent()
-                        .addClass("notInSearch");
-                }
-            },
-        });
+        $(this)
+          .find(".fapicker-icons__search-holder input")
+          .keyup(function () {
+            self.doSearch();
+          });
 
         //type selector
-        $(".fapicker-icons__type-selector li").entwine({
-            onclick: function () {
-                //get the holder
-                let holder = $(".fa-holder");
+        $(this)
+          .find(".fapicker-icons__type-selector li")
+          .click(function () {
+            //set the type to filter by
+            self.setType($(this).data("type"));
+            //hide all other actives
+            $(this).parent().find("li.active").removeClass("active");
+            //add the active class
+            $(this).addClass("active");
+            //run the search
+            self.doFilterType();
+          });
+        this._super();
+      },
+      doSearch: function () {
+        //remove icon not in search class
+        $(this)
+          .find(".fapicker-icons .fapicker-icons__holder li")
+          .removeClass("iconNotSearch");
 
-                //set the type to filter by
-                holder.setType($(this).data("type"));
+        //set newSearch to the val of the input
+        let newSearch =
+          "" + $(this).find(".fapicker-icons__search-holder input").val();
 
-                //hide all other actives
-                $(this).parent().find("li.active").removeClass("active");
+        //filter the list of icons
+        $(this)
+          .find(".fapicker-icons .fapicker-icons__holder li div:first-child")
+          .filter(function (index) {
+            //check to see if the data icon contains the searched word
+            //inverse to hide everything else
+            return !$(this).data("icon").includes(newSearch);
+          })
+          .parent()
+          .addClass("iconNotSearch");
+      },
+      doFilterType: function () {
+        //get the type
+        let type = this.getType() + " ";
 
-                //add the active class
-                $(this).addClass("active");
+        // //remove class
+        $(this)
+          .find(".fapicker-icons .fapicker-icons__holder li")
+          .removeClass("notInSearch");
 
-                //run the search
-                $(".fapicker-icons__search-holder input").doFilterType();
-            },
-        });
+        //do we have a type
+        if (type.trim()) {
+          //filter the list of icons
+          $(this)
+            .find(".fapicker-icons .fapicker-icons__holder li div:first-child")
+            .filter(function (index) {
+              //check to see if the data icon contains the searched word
+              //inverse to hide everything else
+              return !$(this).data("icon").includes(type);
+            })
+            .parent()
+            .addClass("notInSearch");
+        }
+      },
     });
+  });
 })(jQuery);
