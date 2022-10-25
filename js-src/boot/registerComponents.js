d16549908f1b550ee329d9717959e7760b61db7d
DoggersHusky <robert.ce@webbuildersgroup.com>
2022-10-25 12:14:20 -0300

    clean up

--- js-src/boot/registerComponents.js
+++ js-src/boot/registerComponents.js
@@ -1,10 +1,8 @@
 import Injector from 'lib/Injector';
 import FAPickerField from '../components/FAPickerField.jsx';
 
-
 const registerComponents = () => {
   Injector.component.register('FAPickerField', FAPickerField);
-  //Injector.component.register('FAPickerIcon', FAPickerIcon);
 };
 
 export default registerComponents;
